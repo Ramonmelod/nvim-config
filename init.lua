973208165f8525449964c19186e4dbd739ac80fb
ramonmelod <ramonmelo.com@gmail.com>
2024-05-23 11:42:16 -0300

    addition of comments

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@ vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap("i","jk","<ESC>",{noremap = true}) -- the "i" option stands for the moment that this keymap will be active
 
-vim.o.number = true
+vim.o.number = true -- it shows the number of the line in Neovim 
 
-vim.o.mouse = "a"
+vim.o.mouse = "a" --allow the use of mouse in Neovim
 
 require("packer-plugins")
 --vim.bo.shiftwidth = 15
