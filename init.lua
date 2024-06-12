9c1d212863c946d88f94fb0c588b8e885691dc8e
Ramonmelod <ramonmelo.com@gmail.com>
2024-06-12 08:11:20 -0300

    resize of the verticals terminals

--- init.lua
+++ init.lua
@@ -10,13 +10,13 @@ vim.api.nvim_set_keymap("i","jk","<ESC>",{noremap = true}) -- the "i" option sta
 vim.api.nvim_set_keymap("n", "<leader>j", ":NvimTreeToggle<CR>", { noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>l", ":Telescope find_files<CR>", { noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "r", ":redo<CR>", { noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>k", ":ToggleTerm 0 size=40 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 0
-vim.api.nvim_set_keymap("n", "<leader>1", ":ToggleTerm 1 size=40 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 1
-vim.api.nvim_set_keymap("n", "<leader>2", ":ToggleTerm 2 size=40 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 2
-vim.api.nvim_set_keymap("n", "<leader>3", ":ToggleTerm 3 size=40 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 3
-vim.api.nvim_set_keymap("n", "<leader>4", ":ToggleTerm 4 size=40 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 4
-vim.api.nvim_set_keymap("n", "<leader>5", ":ToggleTerm 5 size=40 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 5
-vim.api.nvim_set_keymap("n", "<leader>6", ":ToggleTerm 6 size=40 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 6
+vim.api.nvim_set_keymap("n", "<leader>k", ":ToggleTerm 0 size=60 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 0
+vim.api.nvim_set_keymap("n", "<leader>1", ":ToggleTerm 1 size=60 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 1
+vim.api.nvim_set_keymap("n", "<leader>2", ":ToggleTerm 2 size=60 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 2
+vim.api.nvim_set_keymap("n", "<leader>3", ":ToggleTerm 3 size=60 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 3
+vim.api.nvim_set_keymap("n", "<leader>4", ":ToggleTerm 4 size=60 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 4
+vim.api.nvim_set_keymap("n", "<leader>5", ":ToggleTerm 5 size=60 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 5
+vim.api.nvim_set_keymap("n", "<leader>6", ":ToggleTerm 6 size=60 direction=vertical<CR>", { noremap = true, silent = true}) -- it opens the vert. terminal number 6
 
 vim.api.nvim_set_keymap("n", "<C-j>", ":ToggleTerm 0 size=15 direction=horizontal<CR>", { noremap = true, silent = true}) -- it opens the horizon. terminal number 0
 vim.api.nvim_set_keymap("n", "<C-k>", ":ToggleTerm 1 size=15 direction=horizontal<CR>", { noremap = true, silent = true}) -- it opens the horizon. terminal number 1
